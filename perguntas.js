000b85606c50bff45d9fed41d9f02ed1f8e60762
jhow123123 <164032357+jhow123123@users.noreply.github.com>
2024-10-03 11:39:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,14 @@
+criaCartão(
+    'programaçao',
+    "o que é JavaScritp?",
+)
+CriaCartão(
+    "geografia",
+   "Qual é a capital da França?",
+    " A capital da frança é paris"
+)
+criaCartão(
+    "programação",
+    "O que é uma função",
+    "Uma função é um blocode código que executa alguma tarefa"
+)
